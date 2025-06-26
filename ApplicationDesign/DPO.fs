d6ce3e1e3e2763c9e4df3fbb32f87088a7abacf9
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2025-06-27 01:28:38 +0200

    27-06-2025

--- ApplicationDesign/DPO.fs
+++ ApplicationDesign/DPO.fs
@@ -121,5 +121,4 @@ module WebScraping_DPO =
             
                         return Ok ()
                     }
-
         )
--- Types/Types.fs
+++ Types/Types.fs
@@ -4,9 +4,9 @@ open System.Threading
 
 module Haskell_IO_Monad_Simulation =
 
-    type internal IO<'a> = IO of (unit -> 'a) // wrapping custom type simulating Haskell IO Monad (without the unnecessary monad)
+    type internal IO<'a> = IO of (unit -> 'a) // wrapping custom type simulating Haskell's IO Monad (without the monad, of course)
 
-    let internal runIO (IO action) = action()
+    let internal runIO (IO action) = action ()
 
     //not used yet
     let internal returnIO x = IO (fun () -> x)
