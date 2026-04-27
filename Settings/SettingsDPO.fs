7600b42634bb504c61e85fbb9158b8e3395c0223
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-04-28 00:03:54 +0200

    28-04-2026

--- Settings/SettingsDPO.fs
+++ Settings/SettingsDPO.fs
@@ -2,7 +2,7 @@
 
 module SettingsDPO =
 
-    let [<Literal>] internal pathDpoWeb = @"https://www.dpo.cz"
-    let [<Literal>] internal pathDpoWebTimetablesBus = @"https://www.dpo.cz/pro-cestujici/jizdni-rady/jr-bus.html"  
-    let [<Literal>] internal pathDpoWebTimetablesTrBus = @"https://www.dpo.cz/pro-cestujici/jizdni-rady/jr-trol.html" 
-    let [<Literal>] internal pathDpoWebTimetablesTram = @"https://www.dpo.cz/pro-cestujici/jizdni-rady/jr-tram.html" 
+    let [<Literal>] internal pathDpoWeb = @"https://n.dpo.cz"
+    let [<Literal>] internal pathDpoWebTimetablesBus = @"https://n.dpo.cz/pro-cestujici/jizdni-rady/jr-bus.html"  
+    let [<Literal>] internal pathDpoWebTimetablesTrBus = @"https://n.dpo.cz/pro-cestujici/jizdni-rady/jr-trol.html" 
+    let [<Literal>] internal pathDpoWebTimetablesTram = @"https://n.dpo.cz/pro-cestujici/jizdni-rady/jr-tram.html" 
